746298df1a6630f228acd6abd07d4159e8b10f09
shivam910 <shivams91099@gmail.com>
2021-09-12 12:21:20 +0530

    models & controllers

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,5 +1,22 @@
 // Here we will add all the handlers
+import PostMessage from "../models/PostMessage.js";
 
-export const getPosts = (req, res) => {
-    res.send("This works");
+export const getPosts = async (req, res) => {
+    try {
+      const postMessages = await PostMessage.find();
+      res.status(200).json(postMessages)
+    } catch (error) {
+      res.status(404).json({message: error.message});
+    }
   };
+
+export const createPost = async (req,res) => {
+  const post = res.body;
+  const newPost = new PostMessage(post);
+    try {
+      await newPost.save();
+      res.status(201).json(newPost);
+    } catch (error) {
+      res.status(401).json({message: error.message})
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 // app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
-"mongodb+srv://shivam910:mongo1121025@cluster0.tg51i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+"mongodb+srv://shivam910:@cluster0.tg51i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   // "mongodb+srv://shivam910:@cluster0.tg51i.mongodb.net/example?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
